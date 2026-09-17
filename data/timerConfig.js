ed4c7aa2051fed880830f891ec0c50bbfcb3a4a3
poyopoyo <@>
2026-09-17 16:56:41 +0900

    시간 상수, 할당량 상수 분리

---
+++ data/timerConfig.js
@@ -0,0 +1,19 @@
+export const TIME_CONFIG = {
+  GAME_START: 113,      // 게임 시작 버튼 누르는 기준 시간
+  WAVE_START: 112,       // 웨이브 시작 시간
+  PRE_START: 107,        // 버튼 카운트 시작
+  EXTRA_START: 104,      // 엑스트라 웨이브 시작
+  WAVE_END: -8,
+  FINAL_WAVE_END: -17,
+  EXTRA_WAVE_END: -8,
+  INITIAL_TIME: "-",
+  INITIAL_WAVE: "-",
+};
+
+export const LAST_WAVE = 3;
+
+export const ROOM_TO_STACK = {
+  26: "26-27", 27: "26-27",
+  28: "28-29", 29: "28-29",
+  30: "30",
+};
